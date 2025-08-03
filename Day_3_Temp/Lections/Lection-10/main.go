c06d47a98769d23a757e026138e3714400d61146
Artem Antonian <art902004@gmail.com>
2025-08-03 17:03:47 +0300

    Add files via upload

---
+++ Day_3_Temp/Lections/Lection-10/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	
+// Побитовые операции
+	// & - bit and
+	// | - bit or
+	// ^ - bit xor
+	// >> - сдвиг вправо
+	// << - сдвиг влево
+
+	
+
+}
