0fb98c57a50374aec410670372a7a85f0603d9db
Artem Antonian <art902004@gmail.com>
2025-08-14 21:21:33 +0300

    Add files via upload

--- Day_3_Temp/Homework/task1/m4_t1_hw.go
+++ Day_3_Temp/Homework/task1/m4_t1_hw.go
@@ -1,19 +1,101 @@
-/* 
-	Задача №1
-	Написать функцию, которая расшифрует строку. 
-	code = "220411112603141304"
-	Каждые две цифры - это либо буква латинского алфавита в нижнем регистре либо пробел.
-	Отчет с 00 -> 'a' и до 25 -> 'z', 26 -> ' '(пробел).
-	Вход: строка из цифр. Выход: Текст.
-	Проверка работы функции выполняется через другую строку.
+/*
+Задача №1
+Написать функцию, которая расшифрует строку.
+code = "220411112603141304"
+Каждые две цифры - это либо буква латинского алфавита в нижнем регистре либо пробел.
+Отчет с 00 -> 'a' и до 25 -> 'z', 26 -> ' '(пробел).
+Вход: строка из цифр. Выход: Текст.
+Проверка работы функции выполняется через другую строку.
 
-	Задача №1.1
-	Реализовать и функцию зашифровки
+Задача №1.1
+Реализовать и функцию зашифровки
 
-	codeToString(code) -> "???????' 
+codeToString(code) -> "???????'
 
-	stringToCode("hello") -> "??????"
+stringToCode("hello") -> "??????"
 */
 package main
 
 // Ваш код
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var code, result, word, newResult string
+	fmt.Println("Введите код для расшифровки:")
+	fmt.Scan(&code) //считываем код
+	if len(code)%2 != 0 {
+		fmt.Println("Введите чётное количество чисел")
+		return
+	}
+	codeArrey := strings.Split(code, "") //преобразовываем код в массив
+	//fmt.Println(codeArrey)
+	mapper := make(map[string]string) //создание мапы
+	//генерируем мапу в стиле [00:a] для перевода чисел в буквы
+	for r, i := rune('a'), 0; i <= 26; r, i = r+1, i+1 {
+		mapper[fmt.Sprintf("%02d", i)] = string(r)
+		if i == 26 {
+			mapper[fmt.Sprintf("%02d", i)] = " "
+		}
+	}
+	//fmt.Println(mapper)
+	//Берём первое значение мапы и сравниваем со всеми слайсами через цикл (поэлементно)
+	for a, b := 0, 2; b <= len(codeArrey); a, b = a+2, b+2 { //цикл на генерацию 2-х значений для слайсов
+		for key := range mapper { //цикл мапы с объявлением переменной ключа "00"
+			var codeKey string
+			var subResult string
+			codeSlice := codeArrey[a:b] //взятия срезов из массива кода на базе значений из цикла
+			//fmt.Println("Срез кода:", codeSlice)
+			keyArrey := strings.Split(key, "") //преобразование ключа в массив
+			//fmt.Println("Массив ключа:", keyArrey)
+			for index, value := range keyArrey { //цикл сравнения значений слайса кода и массива ключа
+				//fmt.Println("Значение ключа:", value)
+				//fmt.Println("Значение среза кода:", codeSlice[index])
+				if codeSlice[index] == value { //позначимое сравнение значений массива и слайса через индекс
+					codeKey += value
+					if len(strings.Split(codeKey, "")) == 2 {
+						subResult = codeKey
+					} //else {
+					//fmt.Println("Нет совпадения")
+					//}
+				}
+			}
+			result += mapper[subResult]
+		}
+	}
+	fmt.Println(result)
+
+	fmt.Println("А теперь введите слово маленькими латинскими буквами для зашифровки")
+	input := bufio.NewScanner(os.Stdin)
+	if input.Scan() {
+		word = input.Text()
+	}
+	wordArrey := strings.Split(word, "") //делаем массив из слова
+	mapper2 := make(map[string]string)   //создание мапы для шифрования
+	//генерируем мапу в стиле [a:00] для перевода букв в числа
+	for r, i := rune('a'), 0; i <= 26; r, i = r+1, i+1 {
+		mapper2[string(r)] = fmt.Sprintf("%02d", i)
+		if i == 26 {
+			mapper2[" "] = fmt.Sprintf("%02d", i)
+		}
+	}
+	//fmt.Println(mapper2)
+	//Берём первое значение массива букв и сравниваем со значениями мапы
+	for _, letter := range wordArrey { //цикл вывода букв
+		for key := range mapper2 { //цикл мапы с объявлением переменной значения "a"
+			//fmt.Println(mapper2[key])
+			//fmt.Println(key)
+			//fmt.Println(letter)
+			if letter == key {
+				newResult += mapper2[key]
+				//fmt.Println(newResult)
+			}
+		}
+	}
+	fmt.Println("Зашифрованное послание:", newResult)
+}
--- Day_3_Temp/Homework/task2/m5_t1_hw.go
+++ Day_3_Temp/Homework/task2/m5_t1_hw.go
@@ -1,35 +1,136 @@
 /*
-	Задача №2
-	
-	Вход:
-	Пользователь должен ввести "правильный пароль", состоящий из:
-	цифр, букв латинского алфавита(строчные и прописные) и 
-	специальных символов  special = "_!@#$%^&"
+Задача №2
 
-	Всего 4 набора различных символов.
-	В пароле обязательно должен быть хотя бы один символ из каждого набора.
-	Длина пароля от 8(мин) до 15(макс) символов.
-	Максимальное количество попыток ввода неправильного пароля - 5.
-	Каждый раз выводим номер попытки.
-	*Желательно выводить пояснение, почему пароль не принят и что нужно исправить.
+Вход:
+Пользователь должен ввести "правильный пароль", состоящий из:
+цифр, букв латинского алфавита(строчные и прописные) и
+специальных символов  special = "_!@#$%^&"
 
-	digits = "0123456789"
-	lowercase = "abcdefghiklmnopqrstvxyz"
-	uppercase = "ABCDEFGHIKLMNOPQRSTVXYZ"
-	special = "_!@#$%^&"
+Всего 4 набора различных символов.
+В пароле обязательно должен быть хотя бы один символ из каждого набора.
+Длина пароля от 8(мин) до 15(макс) символов.
+Максимальное количество попыток ввода неправильного пароля - 5.
+Каждый раз выводим номер попытки.
+*Желательно выводить пояснение, почему пароль не принят и что нужно исправить.
 
-	Выход:
-	Написать, что пароль правильный и он принят.
+digits = "0123456789"
+lowercase = "abcdefghiklmnopqrstvxyz"
+uppercase = "ABCDEFGHIKLMNOPQRSTVXYZ"
+special = "_!@#$%^&"
 
-	Пример: 
-	хороший пароль -> o58anuahaunH!
-	хороший пароль -> aaaAAA111!!!
-	плохой пароль -> saucacAusacu8 
+Выход:
+Написать, что пароль правильный и он принят.
 
-	Реализацию оформить через функцию:
-	1. checkPassword(pass string) (bool, errors <- на усмотрение)
+Пример:
+хороший пароль -> o58anuahaunH!
+хороший пароль -> aaaAAA111!!!
+плохой пароль -> saucacAusacu8
 
+Реализацию оформить через функцию:
+1. checkPassword(pass string) (bool, errors <- на усмотрение)
 */
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // Ваш код
+
+func main() {
+	var pass string
+	for i := range 5 {
+		fmt.Scan(&pass)
+		result, errorMsg := checkPassword(pass)
+		if result != true {
+			fmt.Println("У вас сталось", 4-i, "попытки")
+			fmt.Println(errorMsg)
+		} else {
+			fmt.Println("Пароль верный и он принят")
+			break
+		}
+	}
+}
+
+// функция проверки пароля на соотвествие условиям надёжного пароля
+func checkPassword(pass string) (result bool, errorMsg string) {
+	var digits, lwLetters, upLetters, specials bool
+	var digitError, lettersLwError, lettersUpError, specialError string
+	var digitsArrey []string
+	var lettersLwArrey []string
+	var lettersUpArrey []string
+	specialArrey := strings.Split("_!@#$%^&", "")
+	for i := range 10 {
+		digitsArrey = append(digitsArrey, strconv.Itoa(i))
+	}
+	for a, b := 'a', 'A'; a <= 'z'; a, b = a+1, b+1 {
+		lettersLwArrey = append(lettersLwArrey, string(a))
+		lettersUpArrey = append(lettersUpArrey, string(b))
+	}
+	passArrey := strings.Split(pass, "")
+	for _, symbol := range passArrey {
+		for _, digit := range digitsArrey {
+			if digits == true {
+				break
+			}
+			if digit == symbol {
+				digits = true
+				digitError = ""
+				break
+			} else {
+				digitError = "Не хватает цифр"
+			}
+		}
+		for _, lwLetter := range lettersLwArrey {
+			if lwLetters == true {
+				break
+			}
+			if lwLetter == symbol {
+				lwLetters = true
+				lettersLwError = ""
+				break
+			} else {
+				lettersLwError = "Не хватает прописных букв"
+			}
+		}
+		for _, upLetter := range lettersUpArrey {
+			if upLetters == true {
+				break
+			}
+			if upLetter == symbol {
+				upLetters = true
+				lettersUpError = ""
+				break
+			} else {
+				lettersUpError = "Не хватает заглавных букв"
+			}
+		}
+		for _, special := range specialArrey {
+			if specials == true {
+				break
+			}
+			if special == symbol {
+				specials = true
+				specialError = ""
+				break
+			} else {
+				specialError = "Не хватает символов _!@#$%^&"
+			}
+		}
+	}
+	if len(passArrey) > 7 && len(passArrey) < 16 && digits && lwLetters && upLetters && specials {
+		result = true
+	} else if len(passArrey) < 8 {
+		result = false
+		errorMsg = "Пароль должен быть не меньше 8-ми символов"
+	} else if len(passArrey) > 15 {
+		result = false
+		errorMsg = "Пароль должен быть не более 15-ти символов"
+	} else {
+		result = false
+		errorMsg = fmt.Sprintf("%s\n%s\n%s\n%s", digitError, lettersLwError, lettersUpError, specialError)
+	}
+	return result, errorMsg
+}
